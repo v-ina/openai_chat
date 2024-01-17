6aa7bb038d34ce800b91b3662964385a4af743e9
yeonsoo <yeounsoo123@gamil.com>
2024-01-17 16:13:47 +0100

    some correction

--- openainode/openai_api.js
+++ openainode/openai_api.js
@@ -5,22 +5,23 @@ const openai = new OpenAI("OPENAI_API_KEY");
 const aiResponseFromServer = async(req, res, next) => {
   let messages= []
   userContent = `${req.body.userInput}`
-  console.log('유저컨텐츠',req.body);
   messages.push({role : "user", content : userContent})
   try {
     const completion = await openai.chat.completions.create({model: "gpt-3.5-turbo", messages:messages}); 
     req.body.responseData = completion.choices[0].message.content
+    
+    if(userContent.includes('pain au chocolat')){
+      req.body.responseData = `C'EST PAS PAIN AU CHOCOLAT! C'EST CHOCOLATINE!!!!! `
+    }
     next()
   } catch (error) {
     console.error(error);
   }
 }
 
-const aiResponse = (req, res) => {
+const aiResponse = (req, res) => { 
   res.json(req.body.responseData)
 }
-
-
 
 
 module.exports = {aiResponseFromServer, aiResponse}
